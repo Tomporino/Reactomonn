fa505572ffaf592d0d98eb3cb2783bd59e09852f
Tomporino <tomorpeter97@gmail.com>
2021-03-09 18:21:40 +0100

    Add media to PokeCards

--- src/components/MainGrid/PokeCard.js
+++ src/components/MainGrid/PokeCard.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
 import {Typography, Card, CardActions, CardMedia, Button} from '@material-ui/core';
 
-export default function PokeCard({pokemon}){
+export default function PokeCard({pokemon, classes}){
     return ( (pokemon.sprites) ? 
         (<>
             <Card>
-                <CardMedia image={pokemon.sprites.front_default} />
+                <CardMedia className={classes.image} image={pokemon.sprites.front_default} />
                 <CardActions>
                     <Button>{pokemon.name}</Button>
                 </CardActions>
