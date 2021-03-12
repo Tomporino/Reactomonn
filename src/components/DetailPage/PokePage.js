6f5bc1b920d1745aba8950b023f4cf8a0a16c749
Tomporino <tomorpeter97@gmail.com>
2021-03-12 15:16:08 +0100

    Add some style to PokePage

--- src/components/DetailPage/PokePage.js
+++ src/components/DetailPage/PokePage.js
@@ -15,8 +15,8 @@ const useStyles = makeStyles( (theme) => ({
         justifyContent: 'center'
     },
     image: {
-        height: 300,
-        width: 300
+        height: 200,
+        width: 200
     },
     details: {
         width: 300
