5a12fc106fa26376d5886d97c8ae20107297982a
Tomporino <tomorpeter97@gmail.com>
2021-03-31 16:29:03 +0200

    Add bigbluecircle as Home icon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import {makeStyles} from '@material-ui/core/styles';
 import { useState, useEffect } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
+import HomeIcon from '@material-ui/icons/Home';
 
 const useStyles = makeStyles( (theme) => ({
     main: {
@@ -13,6 +14,13 @@ const useStyles = makeStyles( (theme) => ({
     link: {
         color: 'white',
         textDecoration: 'none'
+    },
+    bigbutton: {
+        border: 'solid black 2px',
+        borderRadius: '100px',
+        backgroundColor: 'blue',
+        height: '50px',
+        width: '50px'
     }
 }))
 
@@ -24,7 +32,7 @@ export default function Navbar({loadPrevPage, loadNextPage}) {
         <AppBar className={classes.main} position="static">
             <Toolbar>
                 <Link className={classes.link} to="/">
-                    <Typography>Reactomon</Typography>
+                    <div className={classes.bigbutton}/>
                 </Link>
                 <Link className={classes.link} to="/catched">
                     <Typography>Catched</Typography>
