6215b68ae6d8676e6b0592fb23ec1d77d0fc4d1a
Tomporino <tomorpeter97@gmail.com>
2021-04-07 13:44:25 +0200

    Create redirect onClick event on dropdown buttons

--- src/components/SearchBar/DropdownMenu.js
+++ src/components/SearchBar/DropdownMenu.js
@@ -1,15 +1,29 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Button} from '@material-ui/core';
-
+import { useHistory } from 'react-router-dom'; 
+import getData from '../../api/api';
 
 export default function Dropdown({pokemons}) {
+
+    const [pokemonId, setPokemonId] = useState(null);
+    const history = useHistory();
+
+    const openPokemonPage =  (selectedPokemon) => {
+        getData(selectedPokemon.url).then(data => setPokemonId(data.id));
+    }    
+
+    useEffect(() => {
+        if (pokemonId != null){
+            history.push(`pokemon/${pokemonId}`)
+        }
+    }, [pokemonId])
 
     return (
         <div>
             { pokemons.map(
-                    (pokemon) => {
+                    (pokemon, i) => {
                         return (
-                            <Button>
+                            <Button key={i} onClick={() => openPokemonPage(pokemon)}>
                                 {pokemon.name}
                             </Button>)
                     }
