89d77c9c93362933453b05d82abd451514f6356f
Tomporino <tomorpeter97@gmail.com>
2021-04-10 21:43:58 +0200

    Display evolution next to Details

--- src/components/DetailPage/Evolutions.js
+++ src/components/DetailPage/Evolutions.js
@@ -1,19 +1,36 @@
+import { makeStyles } from '@material-ui/core';
 import React, {useState, useEffect} from 'react';
 import getData from '../../api/api';
+
+
+const useStyles = makeStyles((theme) => ({
+    main: {
+        border: '3px solid black',
+        display: 'flex',
+        flexDirection: 'row'
+
+    },
+    img: {
+        height: 200,
+        width: 200
+    },
+}));
 
 
 export default function Evolutions({pokemon}){
 
     const [evolutions, setEvolutions] = useState(null);
-    const [evolutionChain, setEvolutionChain] = useState(null);
+    const [evolutionChain, setEvolutonChain] = useState(null);
+
+    const classes = useStyles();
 
     async function fetchEvolutionChain() {
         await getData(pokemon.species.url)
-            .then(data => setEvolutionChain(data.evolution_chain))
+            .then(data => fetchEvolutions(data.evolution_chain))
     }
 
-    async function fetchEvolutions() {
-        await getData(evolutionChain.url)
+    async function fetchEvolutions(evolveChain) {
+        await getData(evolveChain.url)
             .then(data => data.chain.evolves_to)
             .then(evolves => {setEvolutions(evolves)})
     }
@@ -33,13 +50,19 @@ export default function Evolutions({pokemon}){
     }
 
     function mapEvolutions() {
+        let evs;
+
         if (evolutions.length == 1) {
-            let evs = getNestedEvolutions(evolutions, []);
-            console.log(evs);
+            evs = getNestedEvolutions(evolutions, []);
         } else if (evolutions.length > 1) {
-            let evs = getUniqueEvolutions();
-            console.log(evs)
+            evs = getUniqueEvolutions();
         }
+
+        Promise.all(evs.map(
+            async evData => {
+                return await getData(`https://pokeapi.co/api/v2/pokemon/${evData.name}`)
+            }
+        )).then(data => setEvolutonChain(data))
     }
 
     useEffect(() => {
@@ -47,19 +70,23 @@ export default function Evolutions({pokemon}){
     }, [])
 
     useEffect(() => {
-        if (evolutionChain){
-            fetchEvolutions();
-        }
-    }, evolutionChain)
-
-    useEffect(() => {
         if (evolutions) {
             mapEvolutions()
         }
     }, evolutions)
 
-
-    return (
-        (evolutionChain) ? <></> : <></>
+    return ((evolutionChain) ? 
+        <div className={classes.main}>
+            {
+                evolutionChain.map(
+                    (evolve, i) => {
+                        return (
+                        <div key={i}>
+                            <img src={evolve.sprites.front_default} className={classes.img}/>
+                        </div>)
+                    } 
+                )
+            }
+        </div> : <></>
     )
 }
